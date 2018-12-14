55266bb7cf963f968aa2d11e4a78a6690733629e
kairat <2deserve@gmail.com>
2018-12-14 20:07:08 +0600

    saving changes

--- src/features/list/List.js
+++ src/features/list/List.js
@@ -3,6 +3,9 @@ import Card from './../card/Card';
 import './List.scss';
 
 class List extends Component {
+  addingNew() {
+    console.log('click');
+  }
   render() {
     const cards = this.props.cards.map(card => {
       return <Card text={card.text} />;
@@ -12,7 +15,7 @@ class List extends Component {
         <div className="list__inner">
           <div className="list__name">{this.props.name}</div>
           <div className="list__cards">{cards}</div>
-          <div className="list__adding">Add new</div>
+          <div className="list__adding-new" onClick={this.addingNew}>Add new</div>
         </div>
       </div>
     );
