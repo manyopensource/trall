4825fe031feb8362d722166f9f1f0202ff0fa739
kairat <2deserve@gmail.com>
2018-12-28 18:35:01 +0600

    bugfix

--- src/reducers/UserReducer.js
+++ src/reducers/UserReducer.js
@@ -10,7 +10,7 @@ const reducer = (state = [], action) => {
     case LOAD_INITIAL_DATA:
       return action.payload.users;
     case CREATE_USER:
-      return state.concat([action.payload]);;
+      return state.concat([action.payload]);
     case UPDATE_USER:
       return { ...state, users: [...state.users, action.payload] };
     case DELETE_USER:
